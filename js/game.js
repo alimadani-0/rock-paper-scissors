a9e91b4a1c13a09da7b2884cab23a1ae3edc0dfc
Ali Madani <54098734+alimadani-0@users.noreply.github.com>
2023-08-20 19:07:03 +0300

    Create function to play a round of the game

--- js/game.js
+++ js/game.js
@@ -21,3 +21,59 @@ function getComputerChoice() {
             return 'Rock'
     }
 }
+
+/*
+playRound:
+takes two inputs: playerChoice and computerChoice
+compares the two inputs
+returns who wins based on comparsion
+
+[X] capitalize playerChoice
+[X] if playerChoice is Rock,
+        player wins against Scissors and loses against Paper
+    ... etc
+[X] generate result string
+
+could use array to check winning order: ['Rock', 'Paper', 'Scissors']
+item before loses, item after wins, if past array index loop (modulus)
+*/
+
+function playRound(playerSelection, computerSelection = getComputerChoice()) {
+    const WON = 'You Win!'
+    const LOST = 'You Lose!'
+    let check = ''
+    let result = ''
+    let winner = ''
+    let loser = ''
+
+    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase()
+    if (playerSelection === computerSelection) {
+        console.log(playerSelection, computerSelection)
+        return "It's a tie!"
+    } else {
+        switch (playerSelection) {
+            case 'Rock':
+                check = 'Scissors';
+                break;
+            case 'Paper':
+                check = 'Rock';
+                break;
+            case 'Scissors':
+                check = 'Paper';
+                break;
+            default:
+                return "Invalid choice, please choose 'Rock', 'Paper' or 'Scissors'."
+        }
+        if (computerSelection === check) {
+            result = WON
+            winner = playerSelection
+            loser = computerSelection
+        } else {
+            result = LOST
+            winner = computerSelection
+            loser = playerSelection
+        }
+
+        return result + " " + winner + " beats " + loser
+    }
+}
