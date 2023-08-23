371e8dd5ba3a7fd69af74c41a17550765f7251de
Ali Madani <54098734+alimadani-0@users.noreply.github.com>
2023-08-23 23:10:50 +0300

    Remove unnecessary comments from js file

--- js/game.js
+++ js/game.js
@@ -1,15 +1,3 @@
-/* 
-getComputerChoice:
-returns either Rock, Paper, or Scissors randomly.
-
-no input, generates random number, match number with a choice, return choice
-
-generate number from 1 to 3
-if number is 1, return rock
-if number is 2, return paper
-if number is 3, return scissors
-*/
-
 function getComputerChoice() {
     let generatedValue = Math.floor(Math.random() * 3) + 1
     switch (generatedValue) {
@@ -21,22 +9,6 @@ function getComputerChoice() {
             return 'Rock'
     }
 }
-
-/*
-playRound:
-takes two inputs: playerChoice and computerChoice
-compares the two inputs
-returns who wins based on comparsion
-
-[X] capitalize playerChoice
-[X] if playerChoice is Rock,
-        player wins against Scissors and loses against Paper
-    ... etc
-[X] generate result string
-
-could use array to check winning order: ['Rock', 'Paper', 'Scissors']
-item before loses, item after wins, if past array index loop (modulus)
-*/
 
 function playRound(playerSelection, computerSelection = getComputerChoice()) {
     const WON = 'You Win!'
@@ -82,25 +54,6 @@ function playRound(playerSelection, computerSelection = getComputerChoice()) {
         }
     }
 }
-
-/*
-game():
-play the game 5 times, keeping a tally of who wins and reporting a winner.
-tie-breaker implemeted
-
-no input, but gets input internally for player choice
-runs a round of the game, announcing winner each round
-updates tally
-repeats till 5 games are reached and no tie in score
-output winner of game
-
-[X] Loop
-[X]     Get user choice
-[X]     Play a round
-[X]     Announce result
-[X]     Update Tally
-[X] Repeat until 5 games and no tie overall
-*/
 
 function getPlayerChoice() {
     const options = ['Rock', 'Paper', 'Scissors']
